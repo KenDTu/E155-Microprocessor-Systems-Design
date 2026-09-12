a6f569aa1e4a479d28833d2c9cea3cda0e50dc14
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-11 21:12:40 -0700

    Update lab1_ktu_tb.sv

--- lab1_ktu_tb.sv
+++ lab1_ktu_tb.sv
@@ -27,20 +27,10 @@ module lab1_ktu_tb();
 		begin
 			clk = 1; #5; clk = 0; #5;
 		end
+	
+	initial begin
 		
-	// ============ TESTING CONNECTIONS TO SUBMODULES ==================== 
-	
-
-	
-	// ============ TESTING FUNCTIONALITY OF HSOSC =======================
-	
-    // ============ TESTING FUNCTIONALITY OF SEVEN SEGMENT =======================
-			
 	//  ========== TESTING TOP LEVEL LOGIC ==========
-	// namely,
-	// assign led[0] = s[1] ^ s[0];
-	// assign led[1] = s[3] && s[2];
-	
 	// test xor 1
         s[1] = 1'b0; s[0] = 1'b0;    			 // setup inputs
         #10;                        // wait required time
@@ -55,7 +45,7 @@ module lab1_ktu_tb();
         assert (led[0] == 1'b1)       // check outputs
             $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
         else 
-            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time);
 			
 	// test xor 3
         s[1] = 1'b1; s[0] = 1'b0;                // setup inputs
@@ -73,7 +63,6 @@ module lab1_ktu_tb();
         else 
             $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
 	
-
 	// test and 1
         s[3] = 1'b0; s[2] = 1'b0;                // setup inputs
         #10;                        // wait required time
@@ -104,8 +93,147 @@ module lab1_ktu_tb();
         assert (led[1] == 1'b1)       // check outputs
             $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
         else 
-            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time);
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time);	
 			
-    #100 $stop;
-  end
+	// ============ TESTING FUNCTIONALITY OF SEVEN SEGMENT =======================
+    // example test 0
+        s = 4'b0000;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b100_0000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+            
+    // test 1
+        s = 4'b0001;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b111_1001)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+
+    // test 2
+        s = 4'b0010;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b010_0100)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test 3
+        s = 4'b0011;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b011_0000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test 4
+        s = 4'b0100;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b001_1001)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+
+    // test 5
+        s = 4'b0101;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b001_0010)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test 6
+        s = 4'b0110;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_0010)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test 7
+        s = 4'b0111;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b111_1000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+
+    // test 8
+        s = 4'b1000;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_0000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test 9
+        s = 4'b1001;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b001_1000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test A
+        s = 4'b1010;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_1000)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+
+    // test B
+        s = 4'b1011;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_0011)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test C
+        s = 4'b1100;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b100_0110)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test D
+        s = 4'b1101;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b010_0001)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+
+    // test E
+        s = 4'b1110;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_0110)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+			
+    // test F
+        s = 4'b1111;                // setup inputs
+        #10;                        // wait required time
+        assert (seg == 7'b000_1110)       // check outputs
+            $display("PASSED! The led controller behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The led controller behaves incorrectly at time: %0t.", $time); 
+	    #100 $stop;
+	end
+	// ============ TESTING CONNECTIONS TO SUBMODULES ==================== 
+	
+
+	
+	// ============ TESTING FUNCTIONALITY OF HSOSC =======================
+	
+    // ============ TESTING FUNCTIONALITY OF SEVEN SEGMENT =======================
+			
+	//  ========== TESTING TOP LEVEL LOGIC ==========
+
+		
 endmodule
